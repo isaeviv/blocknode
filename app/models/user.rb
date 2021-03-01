0bd9c30fdc890c18510bb750750a953c59bf9f86
ilya cherez buk <isaeviv@vivaldi.net>
2021-03-01 17:08:13 +0700

    do sih por emaily

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,9 @@ class User < ActiveRecord::Base
       #zapusk validacij
       #validates eto prosto metod zapuskajuschij validacii
       #to zhe samoe chto i validates(:name, presence: true, i td...)
-
+      #pered sohraneniem ob'ekta vyzyvaem metod before_save i perevodim pochtu v nizhnij registr
+      #self.email oznachet vybor tekuschego usera
+  before_save : { self.email = email.downcase }
   validates :name, presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum: 255},

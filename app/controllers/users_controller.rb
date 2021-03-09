de9336da5390f77c782d6744583248404e1661d4
ilya cherez buk <isaeviv@vivaldi.net>
2021-03-09 17:54:10 +0700

    nevalidnaja registracija

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,5 +11,21 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+    else
+      render 'new'
+    end
+  end
+
+
+
+  private
+    #etot metod tuzhen dlja peredachi na server tol'ko razreshennyh atributov
+    #odin iz sposobov zaschity
+    def params_requare
+      params.requare(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 
 end

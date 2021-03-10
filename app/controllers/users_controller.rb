f07e5a32237818467b1aa5d01fbafbb001e6ad52
ilya cherez buk <isaeviv@vivaldi.net>
2021-03-10 17:56:18 +0700

    registracija

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:succes] = "Добро пожаловать в Blocknode!!!"
+      redirect_to @user
     else
       render 'new'
     end

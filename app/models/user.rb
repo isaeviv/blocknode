3f90b7ce1f2c8f26d153ee95d5bed8403a7cf537
Илья Исаев <78688677+isaeviv@users.noreply.github.com>
2021-05-18 16:48:25 +0700

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,6 +20,7 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: {minimum: 6}, allow_nil:true
 
   #vozvrascaet dajdzhest dannoj stroki
+  #ispol'zuju v fixturah
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST:
                                                   BCrypt::Engine.cost

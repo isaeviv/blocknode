f9e0076d79073e75bbde9531facfdaf3fac2e802
Илья Исаев <78688677+isaeviv@users.noreply.github.com>
2021-05-26 10:04:20 +0700

    Update users_signup_test.rb

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,22 +1,22 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-  test "invalid signup information" do
+  test "invalidnye dannye dlja registracii" do
     get signup_path
     assert_no_difference 'User.count' do
       post users_path, user: { name: "",
                                email: "user@invalid",
-                               password: "foo",
-                               password_confirmation: "bar"}
+                               password: "ras",
+                               password_confirmation: "dva"}
     end
     assert_template 'users/new'
   end
 
-  test "valid signup information" do
+  test "validnye dannye dlja registracii" do
     get signup_path
     assert_difference 'User.count', 1 do
-      post_via_redirect users_path, user: { name:  "Example User",
-                                            email: "user@example.com",
+      post_via_redirect users_path, user: { name:  "Imja Familija",
+                                            email: "imja@example.com",
                                             password:              "password",
                                             password_confirmation: "password" }
     end
